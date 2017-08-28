e470c23b1a77f313ed68241104325706efc091aa
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-08-28 15:04:01 -0400

    feat: setup postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Card, CardText, CardBlock,
+  CardTitle, CardSubtitle } from 'reactstrap';
+
+import { getPostsAPI, getCategoryPostsAPI } from '../actions/postActions';
 
 class PostList extends Component {
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+  
+  componentDidUpdate(prevProps) {
+    if (prevProps.selectedCategory !== this.props.selectedCategory) {
+      this.updatePosts();
+    }
+  }
+  
+
+  updatePosts() {
+    if (this.props.selectedCategory) {
+      this.props.getCategoryPosts(this.props.selectedCategory);
+    } else {
+      this.props.getPosts();
+    }
+  }
+  
+
   render() {
     return (
       <div>
-        Post List
+        {
+          this.props.posts.map((post) => 
+            <Card key={post.id}>
+              <CardBlock>
+                <CardTitle>{post.title}</CardTitle>
+                <CardSubtitle>{post.author}</CardSubtitle>
+                <CardText>
+                  {post.body}
+                </CardText>
+              </CardBlock>
+            </Card>
+          )
+        }
       </div>
     );
   }
 }
 
 PostList.propTypes = {
-
+  selectedCategory: PropTypes.string,
+  getCategoryPosts: PropTypes.func,
+  getPosts: PropTypes.func,
+  posts: PropTypes.array
 };
 
-export default PostList;
+PostList.defaultProps = {
+  posts: []
+};
+
+const mapStateToProps = ({ category, post }) => {
+  if (post.posts) {
+    return {
+      ...category,
+      posts: Object.keys(post.posts).map((postId) => post.posts[postId])
+    };
+  } else {
+    return {
+      ...category
+    };
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    getPosts: getPostsAPI,
+    getCategoryPosts: getCategoryPostsAPI
+  }
+)(PostList);
